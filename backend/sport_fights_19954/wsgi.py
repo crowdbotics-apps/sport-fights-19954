6b58e44349012ef75f69acc211319f5070d4ba97
Crowdbotics <team@crowdbotics.com>
2020-09-02 21:55:22 +0000

    Update models

--- backend/sport_fights_19954/wsgi.py
+++ backend/sport_fights_19954/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sport_fights_19954.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sport_fights_19954.settings")
 
 application = get_wsgi_application()
